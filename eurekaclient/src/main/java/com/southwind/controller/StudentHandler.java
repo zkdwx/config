3ac07b0cd51be6d198ed989d19ac36dec0b63ff3
WangXing <wx2018@mail.ustc.edu.cn>
2019-09-24 23:15:22 +0800

    使用Ribbon实现负载均衡

--- eurekaclient/src/main/java/com/southwind/controller/StudentHandler.java
+++ eurekaclient/src/main/java/com/southwind/controller/StudentHandler.java
@@ -3,6 +3,7 @@ package com.southwind.controller;
 import com.southwind.entity.Student;
 import com.southwind.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -13,6 +14,9 @@ public class StudentHandler {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/findAll")
     public Collection<Student> findAll(){
@@ -37,5 +41,10 @@ public class StudentHandler {
     @DeleteMapping("/deleteById/{id}")
     public void deleteById(@PathVariable("id") long id){
         studentRepository.deleteById(id);
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "当前端口："+this.port;
     }
 }
